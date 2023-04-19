ef192f5b5f780798216d34bcc081cadb9b07adce
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-04-19 17:17:39 +0000

    push

--- console.py
+++ console.py
@@ -144,7 +144,7 @@ class HBNBCommand(cmd.Cmd):
             new_instance = HBNBCommand.classes[classname](**param_dict)
             storage.new(new_instance)
         print(new_instance.id)
-        storage.save()
+        new_instance.save()
 
     def help_create(self):
         """ Help information for the create method """
