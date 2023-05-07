13c1f270f310f33d9b5421fbd782a9d21afc12b3
Morgan <morganokeke@gmail.com>
2023-05-06 18:44:11 -0700

    Fabric script (based on the file 3-deploy_web_static.py) that deletes out-of-date archives, using the function do_clean:

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -67,17 +67,13 @@ def deploy():
 
 
 def do_clean(number=0):
-    if number <= 1:
-        with cd.local('./versions/'):
-            local("ls -lv | rev | cut -f 1 | rev | \
-            head -n +1 | xargs -d '\n' rm -rf")
+    if number == 0 or number == 1:
+        with lcd('./versions/'):
+            local("ls -t | tail -n +1 | xargs -d '\n' rm")
         with cd('/data/web_static/releases/'):
-            run("sudo ls -lv | rev | cut -f 1 | \
-            rev | head -n +1 | xargs -d '\n' rm -rf")
+            run("sudo ls -t | tail -n +1 | xargs -d '\n' rm")
     else:
-        with cd.local('./versions/'):
-            local("ls -lv | rev | cut -f 1 | rev | \
-            head -n +{} | xargs -d '\n' rm -rf".format(number))
+        with lcd('./versions/'):
+            local("ls -t | tail -n +{} | xargs -d '\n' rm".format(number))
         with cd('/data/web_static/releases/'):
-            run("sudo ls -lv | rev | cut -f 1 | \
-            rev | head -n +{} | xargs -d '\n' rm -rf".format(number))
+            run("sudo ls -t | tail -n +{} | xargs -d '\n' rm".format(number))
