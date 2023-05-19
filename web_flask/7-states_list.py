46c01dc89ee160ff62c6531c1e430e109b6da30b
Morgan <morganokeke@gmail.com>
2023-05-19 10:08:48 -0700

    script that starts a Flask web application:

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,7 @@ from models.state import State
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def close_session(self):
     """After each request you must remove the current SQLAlchemy Session
@@ -16,6 +17,7 @@ def close_session(self):
 def state_template():
     """display a HTML page"""
     return render_template('7-states_list.html', states=storage.all(State))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
