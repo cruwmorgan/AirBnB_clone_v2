92df83b8241926de3bd39d54e107c32d00656d04
Morgan <morganokeke@gmail.com>
2023-05-19 06:54:00 -0700

    script that starts a Flask web application:

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -18,7 +18,6 @@ def hbnb():
     return 'HBNB'
 
 
-@app.route('/c/', strict_slashes=False)
 @app.route('/c/<string:text>', strict_slashes=False)
 def text(text=None):
     """display C followed by the value of the text variable"""
@@ -32,7 +31,6 @@ def python_text(text='is cool'):
     return "Python {}".format(text.replace('_', ' '))
 
 
-@app.route('/number/', strict_slashes=False)
 @app.route('/number/<int:n>', strict_slashes=False)
 def int_text(n=None):
     """display n is a number only if n is an integer"""
