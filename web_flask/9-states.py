885ce1a6a675138469039ba14b65977e12559651
Morgan <morganokeke@gmail.com>
2023-05-20 03:27:22 -0700

    script that starts a Flask web application:

---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application:"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(self):
+    """After each request you must remove the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def state_template():
+    """display a HTML page"""
+    return render_template('7-states_list.html', states=storage.all(State))
+
+
+@app.route('/states/<string:id>', strict_slashes=False)
+def state_list_by_id(id=None):
+    """display a HTML page"""
+    sid = 'State.{}'.format(id)
+    return render_template('9-states.html', states=storage.all(State).get(sid)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
